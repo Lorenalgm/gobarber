6fc56ddf42433f588520f1a2f92affcd3692bf55
= <=>
2019-11-06 22:08:52 -0300

    Adicioonado provider

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -5,7 +5,14 @@ class File extends Model {
     super.init({
       name: Sequelize.STRING,
       path: Sequelize.STRING,
-    }, {
+      url: {
+        type: Sequelize.VIRTUAL,
+        get() {
+          return `http://localhost:3000/files/${this.path}`;
+        },
+      },
+    },
+    {
       sequelize,
     });
 
