ef4a646446b49930dc12052cbff8c8f5d8d04292
Lorenalgm <lorena@proesc.com>
2019-11-10 20:18:05 -0300

    Adicionadas validacoes de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -24,10 +25,28 @@ class AppointmentController {
       return res.status(400).json({ error: 'You can only create appointments with providers' });
     }
 
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where:{
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      }
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Appointment is not available' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);
