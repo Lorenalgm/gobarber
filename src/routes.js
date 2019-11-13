dd97ef9676554d558e1f0a78831b3c49d0aa3e81
root <lorena@proesc.com>
2019-11-12 23:51:06 -0300

    Criado update para marcar notificacoes como lidas

--- src/routes.js
+++ src/routes.js
@@ -22,11 +22,15 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedule', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
