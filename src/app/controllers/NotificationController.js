483d354c6caacc5855b0931e215faa0111329b46
Lorenalgm <lorena@proesc.com>
2019-11-14 00:09:38 -0300

    Finalizado metodo de cancelamento de agendamento

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -31,9 +31,6 @@ class NotificationController {
     return res.json(notification);
   }
 
-  async delete(req, res) {
-    return res.json();
-  }
 }
 
 export default new NotificationController();
