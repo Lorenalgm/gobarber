9a6df4380ed11d1af0e3c12a4fdfff8d661caad6
root <lorena@proesc.com>
2019-11-15 14:55:07 -0300

    Configurado envio de email com o nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "829775827e2548",
+    pass: "d9eb6993fe692e",
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
